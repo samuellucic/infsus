65fea2ee2f49b1f321a4e235a51b5ff3a9033a0d
ivanbilobrk <ivan.bilobrk@fer.hr>
2024-05-18 14:35:09 +0200

    ignore

---
+++ DZ03/nis/src/test/kotlin/hr/unizg/fer/nis/NisApplicationTests.kt
@@ -0,0 +1,13 @@
+package hr.unizg.fer.nis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
